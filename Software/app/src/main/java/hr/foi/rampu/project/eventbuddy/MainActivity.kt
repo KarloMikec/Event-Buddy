95a1e13648ba7aad759edb46bda69363c2f0f28b
Karlo Mikec <karlo.mikec18@gmail.com>
2023-11-07 15:42:40 +0100

    Gumb otvara novu aktivnost #8
    Sada pritiskom na gumb, otvara nam se novi zaslon
    Dodan je start_screen.xml i nova kotlin klasa StartScreen

--- Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/MainActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/MainActivity.kt
@@ -2,10 +2,19 @@ package hr.foi.rampu.project.eventbuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+import hr.foi.rampu.project.eventbuddy.activities.StartScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<Button>(R.id.button)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, StartScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/activities/StartScreen.kt
@@ -0,0 +1,13 @@
+package hr.foi.rampu.project.eventbuddy.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import hr.foi.rampu.project.eventbuddy.R
+
+
+class StartScreen : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_screen)
+    }
+}
