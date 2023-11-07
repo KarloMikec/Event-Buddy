57e465e48347d69225aaf30c932eab144f008fc8
Dominik Josipovic <dominik.josipovic@gmail.com>
2023-11-07 16:59:09 +0100

    Kreirani fragmenti za početni ekran #7 Kreirani su prazni layouti za fragmente te su napravljenje klase za te fragmente

---
+++ Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/fragments/HomepageEventsFragment.kt
@@ -0,0 +1,18 @@
+package hr.foi.rampu.project.eventbuddy.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hr.foi.rampu.project.eventbuddy.R
+
+class HomepageEventsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_homepage_events, container, false)
+    }
+}
