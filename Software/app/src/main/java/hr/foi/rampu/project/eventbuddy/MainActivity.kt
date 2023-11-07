616f26e26ada703ccfc8bc9a4dd37ba29f0b2e79
Karlo Mikec <karlo.mikec18@gmail.com>
2023-11-07 19:56:35 +0100

    Dodano pozivanje fragmenta za prijavu na glavnu aktivnost #3

--- Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/MainActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/MainActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
 import hr.foi.rampu.project.eventbuddy.activities.StartScreen
+import hr.foi.rampu.project.eventbuddy.fragments.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container,LoginFragment())
+            .commit()
 
         val buttonClick = findViewById<Button>(R.id.button)
         buttonClick.setOnClickListener {
