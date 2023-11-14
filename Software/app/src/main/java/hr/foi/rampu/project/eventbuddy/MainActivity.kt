94fb78f435b867f7f1852d7c31da284ff874b0ba
Karlo Mikec <karlo.mikec18@gmail.com>
2023-11-14 17:10:25 +0100

    Kreiran fragment za registraciju korisnika #3
    Unutar fragment_registration.xml osmišljen je raspored polja za unos podataka koja će korisnik ispuniti.
    
    Untar RegistrationFragment kreirana je funkcija provjeraUnosa() kojom se provjerava da li je što uneseno u polja za unos.

--- Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/MainActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/MainActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import androidx.fragment.app.Fragment
 import hr.foi.rampu.project.eventbuddy.activities.StartScreen
+import hr.foi.rampu.project.eventbuddy.fragments.FragmentNavigation
 import hr.foi.rampu.project.eventbuddy.fragments.LoginFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentNavigation {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,5 +23,16 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, StartScreen::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun navigateFragment(fragment: Fragment, addToStack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+
+        if (addToStack){
+            transaction.addToBackStack(null)
+        }
+        transaction.commit()
     }
 }
--- Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/fragments/LoginFragment.kt
+++ Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/fragments/LoginFragment.kt
@@ -1,17 +1,37 @@
 package hr.foi.rampu.project.eventbuddy.fragments
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import hr.foi.rampu.project.eventbuddy.R
+
+
 class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        view.findViewById<Button>(R.id.btn_fragment_login_register).setOnClickListener {
+            val navRegister = activity as FragmentNavigation
+            navRegister.navigateFragment(RegistrationFragment(), false)
+        }
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val buttonClick = view.findViewById<Button>(R.id.btn_fragment_login_register)
+        buttonClick.setOnClickListener {
+            val registrationFragment = RegistrationFragment()
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.container, registrationFragment)
+                ?.commit()
+        }
     }
 }
