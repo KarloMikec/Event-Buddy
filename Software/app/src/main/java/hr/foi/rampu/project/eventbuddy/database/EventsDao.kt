c97d2dacae292708f30016946daf283bae9db56e
Dominik Josipović <djosipovi21@student.foi.hr>
2023-12-08 01:34:05 +0100

    Čitanje događaja iz baze #5 implementirano je čitanje događaja iz udaljene baze podataka

--- Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/database/EventsDao.kt
+++ Software/app/src/main/java/hr/foi/rampu/project/eventbuddy/database/EventsDao.kt
@@ -1,5 +1,28 @@
 package hr.foi.rampu.project.eventbuddy.database
 
-class EventsDao {
+import hr.foi.rampu.project.eventbuddy.entities.Event
+import java.text.SimpleDateFormat
+import java.util.Locale
 
+class EventsDao {
+    fun getAllEvents(): List<Event> {
+        // TODO("Napraviti da se ne prikazuju događaji ulogiranog korisnika")
+        val sql = "SELECT * FROM dogadaj"
+        val set = Database.execute(sql)
+        val list: MutableList<Event> = mutableListOf()
+        while(set.next()){
+            val event = Event(
+                name = set.getString("naziv"),
+                date = SimpleDateFormat("yyyy-MM-DD hh:mm:ss", Locale.ENGLISH)
+                    .parse(set.getString("datum"))!!,
+                time = SimpleDateFormat("yyyy-MM-DD hh:mm:ss", Locale.ENGLISH)
+                    .parse(set.getString("datum"))!!,
+                location = set.getString("mjesto"),
+                participants = 0,
+                places = 0
+            )
+            list += event
+        }
+        return list
+    }
 }
